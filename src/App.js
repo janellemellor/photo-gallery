2c38c8bb3ca6648fb33b984ac771ac717a4b3006
janelle mellor <janellemellor@gmail.com>
2020-02-12 11:08:27 -0800

    add filtering

--- src/App.js
+++ src/App.js
@@ -5,17 +5,34 @@ import Header from './Header.js';
 import ImageList from './ImageList.js';
 
 
-class App extends Component {
+export default class App extends Component {
+state = { selected: null };
+
   render() {
-  return (
-    <div>
-      <Header />
-      <ImageList images={images} />
-      
-    </div>
+    const handleChange = e => {
+      this.setState({ selected: e.target.value })};
 
-  );
-}
-}
-
-export default App;
+    const filteredImages = images.filter(image => {
+        if (!this.state.selected) return true;
+        return image.keyword === this.state.selected; });
+    
+    return (
+        <div>
+          <Header />
+          <ImageList images={filteredImages} />
+          <select onChange={handleChange} >
+            <option value='' defaultValue>All</option>
+            <option value='narwhal'>Narwhal</option>
+            <option value='rhino'>Rhino</option>
+            <option value='unicorn'>Unicorn</option>
+            <option value='unilego'>Unilego</option>
+            <option value='triceratops'>Triceratops</option>
+            <option value='markhor'>Markhor</option>
+            <option value='mouflon'>Mouflon</option>
+            <option value='addax'>Addax</option>
+            <option value='chameleon'>Chameleon</option>
+            <option value='lizard'>Lizard</option>
+            <option value='dragon'>Dragon</option>
+          </select>
+        </div>
+  )}}  
