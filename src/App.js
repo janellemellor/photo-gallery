96a5209cfa64c1cc421f4348010c5374c429d728
janelle mellor <janellemellor@gmail.com>
2020-02-13 13:18:41 -0800

    move dropdown to a separate component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import images from './data.js';
 import Header from './Header.js';
 import ImageList from './ImageList.js';
+import Select from './Select.js';
 
 
 export default class App extends Component {
@@ -19,20 +20,7 @@ state = { selected: null };
     return (
         <div>
           <Header />
-          <select onChange={handleChange} >
-            <option value='' defaultValue>All</option>
-            <option value='narwhal'>Narwhal</option>
-            <option value='rhino'>Rhino</option>
-            <option value='unicorn'>Unicorn</option>
-            <option value='unilego'>Unilego</option>
-            <option value='triceratops'>Triceratops</option>
-            <option value='markhor'>Markhor</option>
-            <option value='mouflon'>Mouflon</option>
-            <option value='addax'>Addax</option>
-            <option value='chameleon'>Chameleon</option>
-            <option value='lizard'>Lizard</option>
-            <option value='dragon'>Dragon</option>
-          </select>
+          <Select handleChange={handleChange} />
           <ImageList images={filteredImages} />
          
         </div>
---
+++ src/Select.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+export default class Select extends Component {
+    
+      render() {
+        
+        return (
+            <select onChange={this.props.handleChange} >
+                <option value='' defaultValue>All</option>
+                <option value='narwhal'>Narwhal</option>
+                <option value='rhino'>Rhino</option>
+                <option value='unicorn'>Unicorn</option>
+                <option value='unilego'>Unilego</option>
+                <option value='triceratops'>Triceratops</option>
+                <option value='markhor'>Markhor</option>
+                <option value='mouflon'>Mouflon</option>
+                <option value='addax'>Addax</option>
+                <option value='chameleon'>Chameleon</option>
+                <option value='lizard'>Lizard</option>
+                <option value='dragon'>Dragon</option>
+            </select> 
+      )}}  
